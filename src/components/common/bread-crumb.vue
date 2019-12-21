10a154c1f0d6a57e8c805fd06dde591dd09dc5a5
marta1014 <lovemadenghui@126.com>
2019-12-21 22:35:32 +0800

    创建面包屑组件并全局注册

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,18 @@
+<template>
+<!-- 面包屑组件 -->
+  <el-breadcrumb separator=">">
+      <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+      <!-- 定义具名插槽 -->
+      <el-breadcrumb-item><slot name='title'></slot></el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import asideIn from './aside/aside.vue'
 import headerIn from './header/heaeder.vue'
-
+import breadcrumb from './common/bread-crumb.vue'
 export default {
   install (Vue) {
     Vue.component('asideIn', asideIn)
     Vue.component('headerIn', headerIn)
+    Vue.component('breadcrumb', breadcrumb)// 全局注册面包屑组件
   }
 }
